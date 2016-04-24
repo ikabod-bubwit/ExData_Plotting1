567a5402158987052614b563c1f7af6c225d27be
Broo Root <sl1ng5h0t@yahoo.com>
2016-04-24 11:56:13 -0400

    removed some debugging info and cleared all variables when script executes. some straggler vars became a source of a bug.

--- readPowerConsumptionData.R
+++ readPowerConsumptionData.R
@@ -1,26 +1,24 @@
-# Referemce:
-# http://www.statmethods.net/graphs/line.html
+rm(list=ls())
+
 library(dplyr)
 library(pryr)
 
 readHouseHoldPowerConsumption <- function() {
 	
 	f <- read.csv('household_power_consumption.txt',sep=';', colClasses=c("character","character","double","double","double","double","double","double","numeric"), na.string='?')
-	# f <- read.csv('testdata.txt',sep=';', colClasses=c("character","character","double","double","double","double","double","double","numeric"), na.string='?')
+
 	message('Power Consumption Data Size: ', object_size(f))
 	print(ncol(f))
 	datetime <- as.data.frame(strptime(paste(f$Date, f$Time), "%d/%m/%Y %H:%M:%OS"))
 	names(datetime)<-c('DateTime')
-	fromDate<-strptime('2007-01-02 00:00:00', "%Y-%m-%d %H:%M:%S")
+	fromDate<-strptime('2007-02-01 00:00:00', "%Y-%m-%d %H:%M:%S")
 	toDate<-strptime('2007-02-02 23:59:00', "%Y-%m-%d %H:%M:%S")
 	f<-cbind(f, datetime)
 	# Clear some memory
 	f$Date=NULL
 	f$Time=NULL
-	# print(ncol(f))
+
 	f<-subset(f, f$DateTime > fromDate & f$DateTime < toDate)
 	message('Power Consumption Data Size after filtering: ', object_size(f))
 	return(f)	
 }
-
-# f<-read.csv('testdata.txt',sep=';') %>% select(Date,Time)
