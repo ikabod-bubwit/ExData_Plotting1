571c4be0984aef64141cd5db83f097c4c6873ccd
Broo Root <sl1ng5h0t@yahoo.com>
2016-04-24 13:14:58 -0400

    Plot3 plots done

---
+++ plot3.R
@@ -0,0 +1,30 @@
+# Referemce:
+# http://www.statmethods.net/graphs/line.html
+
+source('readPowerConsumptionData.R')
+f<-readHouseHoldPowerConsumption()
+
+plot3PNG <- function() {
+	png("plot3.png")
+	
+	plot(x=f$DateTime, y=f$Sub_metering_1, type="l", ylab="Energy sub metering", xlab="")
+	lines(x=f$DateTime, y=f$Sub_metering_2, col="red")
+	lines(x=f$DateTime, y=f$Sub_metering_3, col="blue")
+	legend("topright", c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), bty="l", col=c("black","red","blue"), lwd=2, cex=0.7)
+
+	dev.off()	
+}
+
+plot3ToScreen <- function() {
+	plot(x=f$DateTime, y=f$Sub_metering_1, type="l", ylab="Energy sub metering", xlab="")
+	lines(x=f$DateTime, y=f$Sub_metering_2, col="red")
+	lines(x=f$DateTime, y=f$Sub_metering_3, col="blue")
+	legend("topright", c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), bty="l", col=c("black","red","blue"), lwd=2, cex=0.7)
+}
+
+main <- function() {
+	plot3PNG()
+	plot3ToScreen()
+}
+
+main()
